18f6eb704478db1fbe413e3012d7a4dfc705db1d
Source-Code-007 <utsho926@gmail.com>
2025-10-10 01:42:25 +0600

    solving find eventual safe states

--- module_6.5_practice/find_eventual_safe_states.cpp
+++ module_6.5_practice/find_eventual_safe_states.cpp
@@ -4,6 +4,31 @@ using namespace std;
 
 int n, e;
 vector<int> grid[10005];
+bool vis[10005];
+bool pathVis[10005];
+vector<int> safeNodes;
+
+bool DFS(int src)
+{
+    vis[src] = true;
+    pathVis[src] = true;
+
+    for (int child : grid[src])
+    {
+        if (vis[child] && pathVis[child])
+        {
+            return true;
+        }
+
+        if (!vis[child])
+            if (DFS(child))
+                return true;
+    }
+
+    safeNodes.push_back(src);
+    pathVis[src] = false;
+    return false;
+}
 
 int main()
 {
@@ -15,14 +40,28 @@ int main()
         grid[a].push_back(b);
     }
 
+    // memset(safeNodes, -1, sizeof(safeNodes));
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << i << " : ";
+    //     for (int val : grid[i])
+    //     {
+    //         cout << val << " ";
+    //     }
+    //     cout << endl;
+    // }
+
     for (int i = 0; i < n; i++)
     {
-        cout << i << " : ";
-        for (int val : grid[i])
-        {
-            cout << val << " ";
-        }
-        cout << endl;
+        if (!vis[i])
+            DFS(i);
+    }
+
+    sort(safeNodes.begin(), safeNodes.end());
+    for (int val : safeNodes)
+    {
+        cout << val << " ";
     }
 
     return 0;
