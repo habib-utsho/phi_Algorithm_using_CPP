8d3682f0845dca2d4cc0a71fc63d6bfe9799e20b
Source-Code-007 <utsho926@gmail.com>
2025-10-15 20:29:35 +0600

    problem 9 solved

---
+++ module_6.5_practice/10.battleships_in_a_board.cpp
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/battleships-in-a-board/description/
+#include <bits/stdc++.h>;
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
--- module_6.5_practice/9.surronded_regions.cpp
+++ module_6.5_practice/9.surronded_regions.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int r, c;
-int grid[105][105];
-bool vis[105][105];
+char grid[205][205];
+bool vis[205][205];
 
 vector<pair<int, int>> directions = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
@@ -18,6 +18,20 @@ bool valid(int m, int n)
 
 void DFS(int si, int sj)
 {
+    vis[si][sj] = true;
+    // cout << si << " " << sj << " : " << grid[si][sj] << endl;
+
+    for (int i = 0; i < directions.size(); i++)
+    {
+        int ci = si + directions[i].first;
+        int cj = sj + directions[i].second;
+        // cout << ci << " " << cj << " : " << grid[ci][cj] << endl;
+
+        if (valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == 'O')
+        {
+            DFS(ci, cj);
+        }
+    }
 }
 
 int main()
@@ -27,6 +41,33 @@ int main()
         for (int j = 0; j < c; j++)
             cin >> grid[i][j];
 
-    
-        return 0;
+    memset(vis, false, sizeof(vis));
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+        {
+            if (!vis[i][j] && grid[i][j] == 'O' && (i == 0 || j == 0 || i == r - 1 || j == c - 1))
+            {
+                DFS(i, j);
+            }
+        }
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            if (!vis[i][j] && grid[i][j] == 'O')
+                grid[i][j] = 'X';
+        }
+    }
+
+    // grid[3][1] = 'M';
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            cout << grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
