c0a61f805a64410c1c1b36bdc36ec7fef202c8b5
Source-Code-007 <utsho926@gmail.com>
2025-10-16 13:19:39 +0600

    problem 10 battleships in a board solved

--- module_6.5_practice/10.battleships_in_a_board.cpp
+++ module_6.5_practice/10.battleships_in_a_board.cpp
@@ -2,8 +2,57 @@
 #include <bits/stdc++.h>;
 using namespace std;
 
+int r, c;
+char grid[205][205];
+bool vis[205][205];
+
+vector<pair<int, int>> directions = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+
+bool valid(int m, int n)
+{
+    if (m >= r || m < 0 || n >= c || n < 0)
+        return false;
+    return true;
+}
+
+void DFS(int si, int sj)
+{
+    vis[si][sj] = true;
+
+    for (int i = 0; i < directions.size(); i++)
+    {
+        int ci = si + directions[i].first;
+        int cj = sj + directions[i].second;
+
+        if (valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == 'X')
+            DFS(ci, cj);
+    }
+}
+
 int main()
 {
+
+    cin >> r >> c;
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            cin >> grid[i][j];
+
+    memset(vis, false, sizeof(vis));
+    int battleShipCount = 0;
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            if (!vis[i][j] && grid[i][j] == 'X')
+            {
+                DFS(i, j);
+                battleShipCount++;
+            }
+        }
+    }
+
+    cout << battleShipCount << endl;
 
     return 0;
 }
---
+++ module_6.5_practice/11.minimum_number_of_vertices_to_reach_all_nodes.cpp
@@ -0,0 +1,9 @@
+// https : // leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/description/
+#include <bits/stdc++.h>;
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
